98278c043a151603c0836c83e8479d507803af53
Mitchell M <mitchellm1618@gmail.com>
2017-12-01 11:31:22 -0800

    completed TODOs in article.js and comments in articleView.js, added normalize.css

--- daniel-mitchell/scripts/article.js
+++ daniel-mitchell/scripts/article.js
@@ -12,8 +12,9 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
+  // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let source = $('#handlebars').html();
+  let template = Handlebars.compile(source);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -29,12 +30,12 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-  
+  // DONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  return template(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// The reason for including parenthesis around (a,b) but not article or articleObject is that .sort() is being passed multiple parameters instead of one.
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
--- daniel-mitchell/scripts/articleView.js
+++ daniel-mitchell/scripts/articleView.js
@@ -5,7 +5,7 @@ let articleView = {};
 // DONE: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom.
 
 // COMMENT: How do arrow functions affect the context of "this"? How did you determine if a function could be refactored?
-// PUT YOUR RESPONSE HERE
+// Arrow functions refer to the "Window" as "this". If the function directly contains contextual "this" it cannot be refactored. 
 
 articleView.populateFilters = () => {
   $('article').each(function() {
