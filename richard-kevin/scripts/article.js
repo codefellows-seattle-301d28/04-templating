fb253522b379ca7c3d930ebfa847a0ee4d9856a1
Richard Montgomery <rikermonty@gmail.com>
2017-12-01 10:14:43 -0800

    completed TODOs on article.js

--- richard-kevin/scripts/article.js
+++ richard-kevin/scripts/article.js
@@ -1,5 +1,8 @@
 'use strict';
-
+// Created a reference to the HTML of the Handlebars template
+let $articleHtml = $('#article-template').html();
+// Compile our HTML through the Handlebars template
+let articleTemplate = Handlebars.compile($articleHtml);
 let articles = [];
 
 function Article (rawDataObj) {
@@ -12,7 +15,9 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  // TODONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+
+  // We realize we don't need to generate the template each time and so our variables are declared at the top.
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
@@ -29,12 +34,12 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  // TODONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  return articleTemplate(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// Because fat arrow functions require parentheses around multiple arguments.
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
